97ddca1b97cdd4734079b23280383c5dea49e5ae
Markv63 <meverbrugge@yahoo.com>
2020-10-07 18:11:48 -0500

    comitt environment change

--- app_public/src/environments/environment.prod.ts
+++ app_public/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  //apiBaseUrl: 'http://morning-cove-12019.herokuapp.com/api',
-  apiBaseUrl: 'http://localhost:3000/api',
+   
+  apiBaseUrl:'https://desolate-scrubland-2Enter[prise, GITmever5090.herokuapp.com/api',
+  //apiBaseUrl: 'http://localhost:3000/api',
   production: true
 };
